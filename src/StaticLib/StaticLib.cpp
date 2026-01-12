82317198150566db32e0c9b8809e914752ad1681
katusando7 <ryosuke1912@icloud.com>
2026-01-12 10:36:56 +0900

    実装しました。

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,7 +6,47 @@
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
-	// ToDo: クイックソートで並び替えてください
+// 引数チェック
+	if (!begin || !end || begin >= end) 
+	{
+		return false;
+	}
 
-	return false;
+	// 要素の範囲を計算
+	int n = (int)(end - begin);
+	if (n <= 1) return true;
+
+	// ピボット（適当に真ん中付近を選択）
+	int pivot = begin[n / 2].key;
+
+	// パーティション分割
+	item* i = begin;
+	item* j = (item*)end - 1;
+
+	while (i <= j)
+	{
+		while (i->key < pivot) i++;
+		while (j->key > pivot) j--;
+
+		if (i <= j)
+		{
+			// 値の入れ替え
+			item temp = *i;
+			*i = *j;
+			*j = temp;
+			i++;
+			j--;
+		}
+	}
+
+	if (begin < j)
+	{
+		quick_sort(begin, j + 1);
+	}
+	if (i < end)
+	{
+		quick_sort(i, end);
+	}
+
+	return true;
 }
